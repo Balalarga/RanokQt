20bbf79e557c7283a4faac80acb8600e55a6d867
Balalarga <lightman03@mail.ru>
2021-07-16 22:29:02 +0300

    Make small fixes

--- src/main.cpp
+++ src/main.cpp
@@ -5,59 +5,58 @@
 
 using namespace std;
 
-bool ParseArguments(int argc, char *argv[])
+void ParseArguments(int argc, char *argv[])
 {
-    if(argc != 6)
-        return false;
     cout<<"Start compute\n";
 
-    std::string device = argv[1];
-    std::string mode = argv[2];
+    QString device = QString(argv[1]).toLower();
+    QString mode = QString(argv[2]).toLower();
     int recurDepth = stoi(argv[3]);
-    std::string codeFile = argv[4];
+    QString codeFile = argv[4];
+    QString resultFile = argv[5];
 
-
-
-    std::string resultFile = argv[5];
-    if(mode == "Model")
+    if(mode == "model")
         resultFile += ".zdt";
-    else if(mode == "Mimage")
+    else if(mode == "mimage")
         resultFile += ".idt";
 
     Parser parser;
-    parser.SetFile(codeFile);
+    parser.SetFile(codeFile.toStdString());
     Program* program = parser.GetProgram();
     auto args = program->GetSymbolTable().GetAllArgs();
     auto space = SpaceBuilder::Instance().CreateSpace(args[0]->limits, args[1]->limits,
-                                         args[2]->limits, recurDepth);
-    ofstream outputFile(resultFile, ios_base::binary);
+            args[2]->limits, recurDepth);
+    ofstream outputFile(resultFile.toStdString(), ios_base::binary);
     if(!outputFile)
     {
-        cout<<"Result file("<<resultFile<<") error\n";
-        return false;
+        cout<<"Result file("<<resultFile.toStdString()<<") error\n";
+        return;
     }
 
     outputFile<<space->startPoint.x<<space->startPoint.y<<space->startPoint.z;
     outputFile<<space->pointSize.x<<space->pointSize.y<<space->pointSize.z;
     outputFile<<space->GetSize();
-    if(mode == "Model")
+    if(mode == "model")
     {
-        if(device == "Cpu")
+        if(device == "cpu")
             SpaceCalculator::Get().GetModel(*program);
-        else
+        else if(device == "gpu")
             OpenclCalculator::Get().ComputeModel(*program);
-
+        else
+            return;
         for(int i = 0; i < space->GetSize(); ++i)
         {
             outputFile<<space->zoneData->At(i);
         }
     }
-    else if(mode == "Mimage")
+    else if(mode == "mimage")
     {
-        if(device == "Cpu")
+        if(device == "cpu")
             SpaceCalculator::Get().GetMImage(*program);
-        else
+        else if(device == "gpu")
             OpenclCalculator::Get().ComputeImage(*program);
+        else
+            return;
 
         for(int i = 0; i < space->GetSize(); ++i)
         {
@@ -71,30 +70,29 @@ bool ParseArguments(int argc, char *argv[])
     cout<<"Finish\n";
     outputFile.close();
     delete program;
+    SpaceBuilder::Instance().DeleteSpace();
 
-    return true;
+    return;
 }
 
-
-//     0       1              2              3             4               5
-// ./ranok_nd {Cpu|Gpu} {Model|Mimage} {recurDepth} {sourceFile.txt} {resultFile}
 int main(int argc, char *argv[])
 {
-
-    if(ParseArguments(argc, argv))
-    {
-        return 0;
-    }
-    cout<<"You can use me like: \n./ranok_nd {Cpu|Gpu} {Model|Mimage} {recurDepth} {sourceFile.txt} {resultFileName}\n";
-    static_assert (sizeof(cl_double) == sizeof(double), "Type size error cl_double != double");
-
     QApplication a(argc, argv);
 
-    AppWindow w;
-    w.resize(1280, 720);
+    cout<<"Also, you can use me like: \n./Ranok {cpu|gpu} {model|mimage} {recurDepth} {sourceFile.txt} {resultFileName}\n";
+    static_assert (sizeof(cl_double) == sizeof(double), "Type size error cl_double != double");
 
-    qApp->installEventFilter(&w);
+    if(argc == 6)
+        ParseArguments(argc, argv);
+    else
+    {
+        AppWindow w;
+        w.resize(1280, 720);
 
-    w.show();
+        qApp->installEventFilter(&w);
+
+        w.show();
+        return a.exec();
+    }
     return a.exec();
 }
